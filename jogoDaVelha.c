6f58a26e24c0745f547780a96ab0e9b8e11090e1
sluk06 <lokosamuca23@gmail.com>
2024-03-04 20:24:24 -0300

    modificado jogo da velha

---
+++ jogoDaVelha.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int matriz[3][3];
+int num = 1;
+int selecionada;
+int par = 1;
+int venceu = 0;
+int digitados[10];
+int indice = 0;
+
+int imprimirJogo(){
+
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            matriz[i][j] = num;
+            num++;
+        }  
+    }
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            
+            if(matriz[i][j] == selecionada){
+                
+                if(par % 2 == 0){
+                    printf("| X |");
+                }else{
+                    printf("| O |");
+                }    
+                par++;
+                
+            } else {
+                printf("| %d |", matriz[i][j]);
+            }
+            if(matriz[i][j] == 9){
+                num = 1;
+            } 
+        }
+        printf("\n");
+    }
+}
+
+
+void xLetraSelecionada(){
+    int linha;
+    int coluna;
+    
+    printf("\n");
+    printf("\nPor favor escolha um número: ");
+    scanf("\n%d", &selecionada);
+    
+    imprimirJogo();
+
+    digitados[indice] = selecionada;
+    selecionada++;
+}
+
+int main(void){
+    imprimirJogo();
+
+    while(venceu == 0){
+        xLetraSelecionada();
+    }
+}
