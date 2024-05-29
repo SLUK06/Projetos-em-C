ce5d5694778fc0f13a6e09738ffed8cbf79fc6e9
Samuel Tonial <lokosamuca23@gmail.com>
2024-05-29 08:30:20 -0300

    modificado

---
+++ teste25.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x = 2;
+    int y = 2;
+
+    x = x + 1;
+
+    int resultado = x + y;
+
+    printf("resultado : %d", resultado);
+     
+
+    return 0;
+}
